0b6c26297ed342028a324bb5ef256f6327051036
gitTrap <ugo.trapuzzano@gmail.com>
2021-04-23 16:10:08 +0200

    modifications code js et ajout img de fond

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-// fonction pour generer des nombres aleatoirement
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);    
   }
-// fonction  pour generer des couleurs aleatoirement (trouvé sur le net)
-
-  function getRandomColor() {                  
-
-    let letters = '0123456789ABCDEF';
-
-    let color = '#';
-
-    for (let i = 0; i < 6; i++) {
-
-      color += letters[Math.floor(Math.random() * 16)];
-
-    }
-
-    return color;
-
-  }
-// le tableau contenant les citations
-
-
-
+// tableau des citations 
 let citations = ["Chuck Norris est en couleur sur les photos Noir et Blanc.",
 
                 "Chuck Norris a déjà compté jusqu'à l'infini. Deux fois.",
@@ -44,35 +24,22 @@ let citations = ["Chuck Norris est en couleur sur les photos Noir et Blanc.",
 
                 "Chuck Norris a fini son forfait illimité."
             ]
+//fonction pour generer les citations et les couleurs aleatoirement 
+function updateCitation(){
 
-            
-          
+auteur = document.querySelector(".auteur");
+citation = document.querySelector(".citation");  
+citation.innerHTML = citations[getRandomInt(9)];  
+bloc = document.querySelector(".container");
 
-function updateCitation() {  // fonction contenant toutes les modifications des citations
-
-  
-
-    auteur = document.querySelector(".auteur");
-
-    citation = document.querySelector(".citation");  // retourne l'element citation (classe html)
-
-    citation.innerHTML = citations[getRandomInt(9)];  // remplace le texte contenu dans le paragraphe par une des citations du tableau créé ci dessus
-        
-    bloc = document.querySelector(".container");     // retourne l'element container ( classe html)
-
-    citation.style.color = getRandomColor() ;         // change la couleur de la citation aleatoirement au clic du bouton
-
-    bloc.style.backgroundColor = getRandomColor(); // change la couleur du fond au clic du bouton
-    
-    myButton.style.backgroundColor = getRandomColor(); // change la couleur du bouton au clic de celui ci
-
-    auteur.style.color = getRandomColor(); 
+// j'ai utilisé la fonction getramdomint pour generer des nombres aleatoirement entre 0 et 255 , ce qui m'a permis de generer des couleurs aleatoirement a l'aide de rgb
+citation.style.color = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)}) `   
+bloc.style.backgroundColor =  `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)}) ` 
+myButton.style.backgroundColor = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)}) `  
+auteur.style.color = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)}) ` 
 }
 
-let myButton = document.querySelector(".btn");    // retourne l'element bouton
-
-console.log(myButton);   //j'affiche la variable dans la console du navigateur
-
+let myButton = document.querySelector(".btn");        // je selectionne la classe .btn et je la stocke dans une variable
 myButton.addEventListener("click", updateCitation)   // l'action qui se realise au clic du bouton
 
 
@@ -80,3 +47,5 @@ myButton.addEventListener("click", updateCitation)   // l'action qui se realise 
 
 
 
+
+ 
